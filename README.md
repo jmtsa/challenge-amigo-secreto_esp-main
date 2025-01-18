c085e78f1f54324e7a7e76beb00b6d942c6a6663
Jose Manue <jmtsa7@gmail.com>
2025-01-18 11:47:05 -0600

    Actualizacion del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # challenge-amigo-secreto_esp-main
+
 <h1>Amigo secreto</h1>
+
+Este es el primer proyecto de ONE Education y alura LAtam
+
+Consiste en realizar un juego para saber quién es el amigo secreto de una lista generada por el usuario.
+
+Esta lista se usa para generar un nombre aleatorio
+
+Cuenta con Validaciones en caja de entrada, así cómo si se trata de generar un amigo secreto si aún noy una lista.
+
+funciones empleadoas:
+
+- amigoAleatorio
+  De la lista creada genera aleatoriamente un
+
+  -sortearAmigo
+  llama a la función "amigo aleatorio para obtener el amigo"
+
+  -limpiaMensajeSinAmigosAun
+  Se usa válidando que si se trata de generar un amigo secretosin haber una lista, muestra un mensaje en pantalla y se usa al momento de introducir el primer nombre para borrar el mensaje y que empice a generar la lista de nombres.
+
+  -limpiarCampoDeEntrada
+  Cada vez que se agrega un nombre, deja el campo vacío para el siguiente nombre.
+
+  -agregarAmigo
+  Agrega el nombre que se capturó tanto a el arreglo de amigos, así como para que lo muestre en pantalla.
--- app.js
+++ app.js
@@ -1,2 +1,53 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigo = [];
+//Array de amigos
+let amigos = [];
+//Captura lo que hay en el cuadro de texto
+let amigo = document.getElementById('amigo');
+//Prepara la lista a mostrar en pantalla
+let listaAmigos = document.getElementById('listaAmigos');
+
+//Agrega el amigo a la pantalla y al array amigos;
+function agregarAmigo() {
+  //impiarListaAmigos();
+
+  let amigoTexto = amigo.value.trim();
+
+  if (amigoTexto !== '') {
+    let li = document.createElement('li');
+
+    limpiaMensajeSinAmigosAun();
+
+    amigos.push(amigoTexto);
+
+    li.textContent = amigoTexto;
+    listaAmigos.appendChild(li);
+  }
+  //Llama función para limpiar campo de entrada
+  limpiarCampoDeEntrada();
+}
+
+function limpiarCampoDeEntrada() {
+  amigo.value = '';
+}
+
+function limpiaMensajeSinAmigosAun() {
+  if (listaAmigos.textContent == 'No hay lista de amigos') {
+    listaAmigos.innerHTML = '';
+  }
+}
+
+function sortearAmigo() {
+  let mejorAmigo = amigoAleatorio(amigos);
+
+  return (listaAmigos.innerHTML = mejorAmigo);
+}
+
+//Genera el amigo secreto
+function amigoAleatorio(lista) {
+  if (amigos.length) {
+    let item = Math.floor(Math.random() * lista.length);
+    return lista[item];
+  } else {
+    return 'No hay lista de amigos';
+  }
+}
